848cd0058763e2482179d20ce0721f2ff3be1e4f
amandasmcvey <amandasmcvey@ufl.edu>
2015-11-17 11:03:41 -0500

    Scores Update
    still not as clean looking as can be

--- Assets/Cubiquity/Examples/SharedAssets/Scripts/mygui.cs
+++ Assets/Cubiquity/Examples/SharedAssets/Scripts/mygui.cs
@@ -6,11 +6,15 @@ public class mygui : MonoBehaviour {
 
 	string name; 
 	List<Scores> highscores;
+	Rect lastRect;
+	
 	// Use this for initialization
 	void Start () {
 		name = "bob";
 		highscores = new List<Scores>();
 		HighScoreManager._instance.SaveHighScore(name, float.Parse(TrackingGlobalVars.truncatedYellow.ToString()), float.Parse(TrackingGlobalVars.truncatedBrown.ToString()), float.Parse(TrackingGlobalVars.truncatedBlack.ToString()), float.Parse(TrackingGlobalVars.truncatedWhite.ToString()));
+		
+		
 	}
 	
 	// Update is called once per frame
@@ -22,34 +26,55 @@ public class mygui : MonoBehaviour {
 	
 	void OnGUI()
 		{
+		
+		var centeredStyle = GUI.skin.GetStyle("Label");
+		centeredStyle.alignment = TextAnchor.UpperCenter;
+		
 		highscores = HighScoreManager._instance.GetHighScore();
 		
-		  GUI.Label(new Rect(0,0,100,100), ("Yellow: " + TrackingGlobalVars.truncatedYellow.ToString() + "%  Brown: " + TrackingGlobalVars.truncatedBrown.ToString() + "%  Black: " + TrackingGlobalVars.truncatedBlack + "%  White: " + TrackingGlobalVars.truncatedWhite + "%"));
+		 GUILayout.Space(25);
 		  
-		  GUILayout.Space(60);
-         
-         GUILayout.BeginHorizontal();
-         GUILayout.Label("Name",GUILayout.Width(Screen.width/5));
-         GUILayout.Label("Scores",GUILayout.Width(Screen.width/5));
-         GUILayout.Label("Scores",GUILayout.Width(Screen.width/5));
-         GUILayout.Label("Scores",GUILayout.Width(Screen.width/5));
-         GUILayout.Label("Scores",GUILayout.Width(Screen.width/5));
+		 GUILayout.BeginHorizontal();
+         //GUILayout.Label("Name",GUILayout.Width(Screen.width/5));
+         GUILayout.Label("Black %",GUILayout.Width(Screen.width/4));
+         GUILayout.Label("Brown %",GUILayout.Width(Screen.width/4));
+         GUILayout.Label("Yellow %",GUILayout.Width(Screen.width/4));
+         GUILayout.Label("White %",GUILayout.Width(Screen.width/4));
          GUILayout.EndHorizontal();
-         
-         GUILayout.Space(25);
-         
-         foreach(Scores _score in highscores)
-         {
-             GUILayout.BeginHorizontal();
-             GUILayout.Label(_score.name,GUILayout.Width(Screen.width/5));
-             GUILayout.Label(""+_score.yellowScore,GUILayout.Width(Screen.width/5));
-             GUILayout.Label(""+_score.brownScore,GUILayout.Width(Screen.width/5));
-             GUILayout.Label(""+_score.blackScore,GUILayout.Width(Screen.width/5));
-             GUILayout.Label(""+_score.whiteScore,GUILayout.Width(Screen.width/5));
-             GUILayout.EndHorizontal();
-         }
 		  
-		  GUI.Button(new Rect(165,320,145,30),"Back to Main Menu");
+		 GUILayout.BeginHorizontal();
+		 //GUILayout.Label(_score.name,GUILayout.Width(Screen.width/5));
+		 GUILayout.Label(""+TrackingGlobalVars.truncatedBlack,GUILayout.Width(Screen.width/4));
+		 GUILayout.Label(""+TrackingGlobalVars.truncatedBrown,GUILayout.Width(Screen.width/4));
+		 GUILayout.Label(""+TrackingGlobalVars.truncatedYellow,GUILayout.Width(Screen.width/4));
+		 GUILayout.Label(""+TrackingGlobalVars.truncatedWhite,GUILayout.Width(Screen.width/4));
+		 GUILayout.EndHorizontal(); 
+		  
+		  GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(1));
+        
+		 GUILayout.BeginHorizontal();
+		 //GUILayout.Label("Name",GUILayout.Width(Screen.width/5));
+		 GUILayout.Label("Black %",GUILayout.Width(Screen.width/4));
+		 GUILayout.Label("Brown %",GUILayout.Width(Screen.width/4));
+		 GUILayout.Label("Yellow %",GUILayout.Width(Screen.width/4));
+		 GUILayout.Label("White %",GUILayout.Width(Screen.width/4));
+		 GUILayout.EndHorizontal();
+		 
+		 foreach(Scores _score in highscores)
+		 {
+			 GUILayout.BeginHorizontal();
+			 //GUILayout.Label(_score.name,GUILayout.Width(Screen.width/5));
+			 GUILayout.Label(""+_score.blackScore,GUILayout.Width(Screen.width/4));
+			 GUILayout.Label(""+_score.brownScore,GUILayout.Width(Screen.width/4));
+			 GUILayout.Label(""+_score.yellowScore,GUILayout.Width(Screen.width/4));
+			 GUILayout.Label(""+_score.whiteScore,GUILayout.Width(Screen.width/4));
+			 GUILayout.EndHorizontal();
+		 }
+		  GUI.Button(new Rect((Screen.width / 2) - Screen.width / 4, 340, 130, 20),"Back to Main Menu");
+		  //GUI.Button(new Rect(165,320,145,30),"Back to Main Menu");
+		  
+		 
+		  
 		}
 }
 
